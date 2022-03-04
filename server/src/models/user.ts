3cc7cf0ec0b22a1a64fb569c7ebcdbc1a9740ad1
Vasilis CodeX <vasilis.tris@gmail.com>
2022-03-04 20:19:04 +0200

    postgres connection class and getusers model function

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,4 +1,6 @@
-import { IUser, IUserProperties } from '../interfaces/user';
+import PostgreSQL from '../connections/postgres';
+import ObjectHandler from '../helpers/objectHandler';
+import { IListOfUsers, IUser, IUserFilters, IUserProperties } from '../interfaces/user';
 
 export default class UserModel implements IUser {
     constructor(user: IUserProperties = {}) {
@@ -10,6 +12,24 @@ export default class UserModel implements IUser {
         this.setUsername(user.username ? user.username : '');
         this.setPassword(user.password ? user.password : '');
         this.setCreatedAtStamp(user.created_at ? user.created_at : 0);
+    }
+
+    async getUsers(filters: IUserFilters = {}): Promise<IListOfUsers | boolean> {
+        try {
+            const wherePart = ObjectHandler.objectToSQLParams(ObjectHandler.getResource(this), ' AND ');
+            let results: IListOfUsers = [];
+
+            const query = await PostgreSQL.client.query(`SELECT * FROM users 
+                ${wherePart ? `WHERE ${wherePart}` : ''}
+                ${'orderby' in filters ? `ORDER BY ${filters.orderby}` : ''}
+                ${'limit' in filters ? `LIMIT ${filters.limit}` : ''}`);
+            if (query.rowCount === 0) throw Error();
+
+            results = query.rows;
+            return results;
+        } catch (error) {
+            return false;
+        }
     }
 
     getId(): number {
