47b5783086d23a573dfcdf7a28ed6afa3a51afea
Vasilis CodeX <vasilis.tris@gmail.com>
2022-03-04 15:54:38 +0200

    added build with gulp

---
+++ server/dist/src/bin/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const arg_1 = __importDefault(require("arg"));
+const server_1 = __importDefault(require("./server"));
+const path_1 = __importDefault(require("path"));
+const args = (0, arg_1.default)({ '--env': String });
+let confFile = '.env';
+if (args['--env'] == 'dev')
+    confFile = '.env.dev';
+require('dotenv').config({ path: path_1.default.join(__dirname, `../config/${confFile}`) });
+const server = new server_1.default();
+server.run(Number(process.env.PORT), () => console.log(`Server started on port: ${process.env.PORT}`));
