1d34aac504f4d34776948c53594fc49284c50797
Vasilis CodeX <vasilis.tris@gmail.com>
2022-03-06 14:31:33 +0200

    env files & basic structure

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_PORT: process.env.NEXT_PUBLIC_API_PORT
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
 }
 
 module.exports = nextConfig
