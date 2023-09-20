d74ad5d8e921c94db016f1443bfa04c25e230978
Vasilis CodeX <vasilis.tris@gmail.com>
2023-09-20 23:26:10 +0300

    reinit project

--- server/dist/src/interfaces/rate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RateGlobals = void 0;
-var RateGlobals;
-(function (RateGlobals) {
-    RateGlobals[RateGlobals["QUERY_LENGTH"] = 10] = "QUERY_LENGTH";
-    RateGlobals["QUERY_ORDER_FIELD"] = "movieid";
-    RateGlobals["QUERY_SORT_METHOD"] = "ASC";
-})(RateGlobals = exports.RateGlobals || (exports.RateGlobals = {}));
